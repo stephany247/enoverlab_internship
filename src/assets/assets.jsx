82b1854267783aebdfd6313edef9ebf55512cd00
Onyinye Oguocha <stephanieoguocha@gmail.com>
2025-07-01 16:10:26 +0100

    Add new SVG logo and profile picture
    - Introduced a new SVG logo file (jermai.svg) with detailed vector graphics.
    - Added a new profile picture (profile-pic.png) to enhance user interface visuals.

--- src/assets/assets.jsx
+++ src/assets/assets.jsx
@@ -1,7 +1,9 @@
-import logo from "./logo.png";
-
+import logo from "./jermai.svg";
+import bannerImage from "./banner.svg";
+import profilePic from "./profile-pic.png";
 
 export const assets = {
   logo,
- 
+  bannerImage,
+  profilePic
 };
